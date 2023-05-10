6ce0f3713c8d2a3ffa0b29d3ae3d9de59e2b77b4
VACostin <virlan.costin.andrei@gmail.com>
2023-05-10 16:30:16 +0300

    Solved Fundamentals Part 1

---
+++ javascript.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+let value1 = 23;
+let value2 = 97;
+
+/* 1 */
+console.log(value1 + value2);
+/* 2 */
+console.log(13 + 17 + 23 + 29 + 31 + 37);
+/* 3 */
+console.log((4 + 6 + 9) / 77);
+/* 4 */
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b)
+/* 5 */
+const MAX=57;
+const ACTUAL=MAX-13;
+const PERCENTAGE=ACTUAL/MAX;
+console.log(PERCENTAGE);
